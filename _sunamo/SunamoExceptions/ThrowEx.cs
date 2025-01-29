08ceb5ee254d01e7c9b5b7524d7cbf3fb8e445df
sunamo.cz <radek.jancik@sunamo.cz>
2025-01-29 22:10:55 +0100

    Remove another unused methods

--- _sunamo/SunamoExceptions/ThrowEx.cs
+++ _sunamo/SunamoExceptions/ThrowEx.cs
@@ -2,8 +2,6 @@ namespace SunamoXlfKeys._sunamo.SunamoExceptions;
 internal partial class ThrowEx
 {
 
-    internal static bool IsNullOrEmpty(string argName, string argValue)
-    { return ThrowIsNotNull(Exceptions.IsNullOrWhitespace(FullNameOfExecutedCode(), argName, argValue, true)); }
 
     #region Other
     internal static string FullNameOfExecutedCode()
