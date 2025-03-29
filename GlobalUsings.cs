46f8a98abd89fa6d23c84d080ef9ed15290418a4
sunamo.cz <radek.jancik@sunamo.cz>
2025-03-29 19:30:46 +0100

    25.3.29.1 Updated nuget to latest versions

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -9,7 +9,6 @@ global using System.Reflection;
 global using System.Net;
 global using System.Diagnostics.CodeAnalysis;
 global using SunamoXlfKeys._sunamo;
-
 global using ILogger = Microsoft.Extensions.Logging.ILogger;
 global using Microsoft.Extensions.Logging;
 global using NullLogger = Microsoft.Extensions.Logging.Abstractions.NullLogger;
